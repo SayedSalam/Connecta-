c11b8512224bdcb723a6385398e821b0ad019912
Sayed Sallam <125590519+SayedSalam@users.noreply.github.com>
2026-07-07 16:42:54 +0300

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const repoName = 'Connecta-';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
 };
 export default nextConfig;
